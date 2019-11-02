0c6b2a90e45bedb8c01ef4f8da3d5127fccd929c
Tanha-Shah <tanhashah95@gmail.com>
2019-11-02 14:10:25 -0400

    Basic draggable pie chart for handling 3 parameters (Safety, Travel cost, Airbnb cost)

--- public/js/map.js
+++ public/js/map.js
@@ -58,7 +58,7 @@ pointsOfInterest = [
 	}, 
 ];
 
-
+$(document).ready(setupPieChart);
 pointsOfInterest.forEach(function(point){
 	var marker = L.marker(point.latLong).addTo(map);
 	marker.bindPopup("<div class='poi_popup itemNotInItineary' id='point_holder_"+point.id+"'>\
@@ -106,6 +106,98 @@ $('.dropdown-item').click(function() {
   		$('<div id="itineary-day-'+i+'">Day '+i+'<ul></ul></div>').appendTo('#itineary');
 	}
 });
+
+function setupPieChart() {
+		var dimensions = ['Safety', 'Travel Cost', 'Airbnb Cost'];
+		var randomProportions = [0.34,0.33,0.33];
+        var proportions = dimensions.map(function(d,i) { return {
+            label: d,
+            proportion: randomProportions[i],
+            collapsed: false,
+            format: {
+                label: d.charAt(0).toUpperCase() + d.slice(1) // capitalise first letter
+            }
+        }});
+
+
+        var setup = {
+            canvas: document.getElementById('piechart'),
+            radius: 0.9,
+            collapsing: true,
+            proportions: proportions,
+            drawSegment: drawSegmentOutlineOnly,
+            onchange: onPieChartChange
+        };
+
+        var newPie = new DraggablePiechart(setup);
+
+        function drawSegmentOutlineOnly(context, piechart, centerX, centerY, radius, startingAngle, arcSize, format, collapsed) {
+
+            if (collapsed) { return; }
+
+            // Draw segment
+            context.save();
+            var endingAngle = startingAngle + arcSize;
+            context.beginPath();
+            context.moveTo(centerX, centerY);
+            context.arc(centerX, centerY, radius, startingAngle, endingAngle, false);
+            context.closePath();
+
+            context.fillStyle = '#f5f5f5';
+            context.fill();
+            context.stroke();
+            context.restore();
+
+            // Draw label on top
+            context.save();
+            context.translate(centerX, centerY);
+            context.rotate(startingAngle);
+
+            var fontSize = Math.floor(context.canvas.height / 25);
+            var dx = radius - fontSize;
+            var dy = centerY / 10;
+
+            context.textAlign = "right";
+            context.font = fontSize + "pt Helvetica";
+            context.fillText(format.label, dx, dy);
+            context.restore();
+        }
+
+        function onPieChartChange(piechart) {
+
+            var table = document.getElementById('proportions-table');
+            var percentages = piechart.getAllSliceSizePercentages();
+
+            var labelsRow = '<tr>';
+            var propsRow = '<tr>';
+            for(var i = 0; i < proportions.length; i += 1) {
+                labelsRow += '<th>' + proportions[i].format.label + '</th>';
+
+                var v = '<var>' + percentages[i].toFixed(0) + '%</var>';
+                var plus = '<div id="plu-' + dimensions[i] + '" class="adjust-button" data-i="' + i + '" data-d="-1">&#43;</div>';
+                var minus = '<div id="min-' + dimensions[i] + '" class="adjust-button" data-i="' + i + '" data-d="1">&#8722;</div>';
+                propsRow += '<td>' + v + plus + minus + '</td>';
+            }
+            labelsRow += '</tr>';
+            propsRow += '</tr>';
+
+            table.innerHTML = labelsRow + propsRow;
+
+            var adjust = document.getElementsByClassName("adjust-button");
+
+            function adjustClick(e) {
+                var i = this.getAttribute('data-i');
+                var d = this.getAttribute('data-d');
+
+                piechart.moveAngle(i, (d * 0.1));
+            }
+
+            for (i = 0; i < adjust.length; i++) {
+                adjust[i].addEventListener('click', adjustClick);
+            }
+
+        }
+    }
 
 // If you want to use D3 instead of native leaflet
 // var cities = [];
